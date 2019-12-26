3c14450715e2ad175f1564432ca0aa75b2d0be18
Ricky Arora <your_email_here>
2019-12-25 21:19:13 -0500

    updatin

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_12_22_222306) do
+ActiveRecord::Schema.define(version: 2019_12_26_020933) do
 
   create_table "users", force: :cascade do |t|
     t.string "name"
